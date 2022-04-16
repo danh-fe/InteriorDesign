b16bd59f5f35195f9b91516bba0379982de89382
Danh Nguyen <danhntc.fe@gmail.com>
2022-04-16 15:28:13 +0700

    Change File

--- assets/js/showMenu.js
+++ assets/js/showMenu.js
@@ -1,10 +1,10 @@
 function onClickMenu() {
-    let menu = document.getElementById('show-menu');
-    if (menu.classList.contains("show")) {
-        menu.classList.remove("show")
-        menu.classList.add("hide")
-    } else {
-        menu.classList.remove("hide")
-        menu.classList.add("show")
-    }
+  let menu = document.getElementById("show-menu");
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+    menu.classList.add("hide");
+  } else {
+    menu.classList.remove("hide");
+    menu.classList.add("show");
+  }
 }
